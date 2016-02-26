d5348d641644e1621d0ee9652c33a3f08e7a43d3
lzxuan <421051332@qq.com>
2016-02-26 14:20:14 +0800

    worker1 创建 person类

---
+++ GitDemo/GitDemo/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  GitDemo
+//
+//  Created by lzxuan on 16/2/26.
+//  Copyright © 2016年 lzxuan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ GitDemo/GitDemo/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  GitDemo
+//
+//  Created by lzxuan on 16/2/26.
+//  Copyright © 2016年 lzxuan. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
