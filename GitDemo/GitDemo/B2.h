01124b290eef143eb71ef5620ae37d09493bc8cb
lzxuan <421051332@qq.com>
2016-02-26 15:13:44 +0800

    222

---
+++ GitDemo/GitDemo/B2.h
@@ -0,0 +1,13 @@
+//
+//  B2.h
+//  GitDemo
+//
+//  Created by lzxuan on 16/2/26.
+//  Copyright © 2016年 lzxuan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface B2 : NSObject
+
+@end
---
+++ GitDemo/GitDemo/B2.m
@@ -0,0 +1,13 @@
+//
+//  B2.m
+//  GitDemo
+//
+//  Created by lzxuan on 16/2/26.
+//  Copyright © 2016年 lzxuan. All rights reserved.
+//
+
+#import "B2.h"
+
+@implementation B2
+
+@end
