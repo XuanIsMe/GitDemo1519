8e3cde3b679a9228ab4fd7657a220ed313622f10
lzxuan <421051332@qq.com>
2016-02-26 14:25:09 +0800

    worker2 创建一个student 类

---
+++ GitDemo/GitDemo/Student.h
@@ -0,0 +1,13 @@
+//
+//  Student.h
+//  GitDemo
+//
+//  Created by lzxuan on 16/2/26.
+//  Copyright © 2016年 lzxuan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Student : NSObject
+
+@end
---
+++ GitDemo/GitDemo/Student.m
@@ -0,0 +1,13 @@
+//
+//  Student.m
+//  GitDemo
+//
+//  Created by lzxuan on 16/2/26.
+//  Copyright © 2016年 lzxuan. All rights reserved.
+//
+
+#import "Student.h"
+
+@implementation Student
+
+@end
