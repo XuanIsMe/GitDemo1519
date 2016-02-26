ccca17db416c80f9fe2d90f55f1af765c9c3352f
lzxuan <421051332@qq.com>
2016-02-26 15:07:00 +0800

    2worker2

---
+++ GitDemo/GitDemo/Student2.m
@@ -0,0 +1,13 @@
+//
+//  Student2.m
+//  GitDemo
+//
+//  Created by lzxuan on 16/2/26.
+//  Copyright © 2016年 lzxuan. All rights reserved.
+//
+
+#import "Student2.h"
+
+@implementation Student2
+
+@end
